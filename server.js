9d0015037df8a2bc4c43c381770cb89ad4943608
RamPreetham <maruthiram957@gmail.com>
2024-05-10 21:33:36 -0400

    Updated according to the requirement

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
+mongoose.connect('mongodb://localhost/urlShortner')
+  
 app.set('view engine','ejs')
+app.use(express.urlencoded({extended: false}))
+app.get('/',async (req,res)=>{
+    const shorturls = await ShortUrl.find()
+    res.render('index',{shorturls: shorturls})
+})
 
-app.get('/',(req,res)=>{
-    res.render('index')
+app.post('/shortUrls',async(req,res)=>{
+   await ShortUrl.create({
+        full: req.body.fullUrl
+    })
+    res.redirect('/')
+
 })
 app.listen(process.env.PORT || 8080);
